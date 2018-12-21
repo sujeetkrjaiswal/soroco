15a79ef004df78c2281d115b51cbcb9ae39e1ba7
Sujeet Kumar Jaiswal <Sujeet_Jaiswal@outlook.com>
2018-12-21 18:42:59 +0530

    Adding build

---
+++ precache-manifest.4fd614715c5143fd75df827639fb617e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f34384edc8a17444043",
+    "url": "./static/css/main.d9d3f66c.chunk.css"
+  },
+  {
+    "revision": "6f34384edc8a17444043",
+    "url": "./static/js/main.6f34384e.chunk.js"
+  },
+  {
+    "revision": "c0bcd748e8be0b5f6843",
+    "url": "./static/js/1.c0bcd748.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0c8fa13f27dd2bac6b2707a98307bdc1",
+    "url": "./index.html"
+  }
+];
--- src/App/components/Header/Header.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Header.css";
-
-export default function Header(props) {
-  return <header className="src-header">Soroco</header>;
-}
